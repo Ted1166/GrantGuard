37ddf9bb3a9964600d4e3c5a8ae0397387ee9e40
Ted1166 <ogolated00@gmail.com>
2026-06-15 13:26:44 +0300

    fix: nextjs

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   turbopack: {},
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-native-async-storage/async-storage': false,
+    }
+    return config
+  },
 }
 
 export default nextConfig
